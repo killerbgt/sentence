d66dc109f3cebd4e11fea8740c02ac1d6b814799
Jeremy Altman <github@jeremyaltman.com>
2016-08-04 12:37:14 -0400

    Implement Sentence#reverse method

--- lib/sentence.rb
+++ lib/sentence.rb
@@ -1,5 +1,23 @@
 require "sentence/version"
 
 module Sentence
-  # Your code goes here...
+  class SentenceError < StandardError; end
+  class InvalidString < SentenceError; end
+
+  def self.reverse(sentence)
+    # raise exception if provided a non-string
+    raise InvalidString if !sentence.is_a?(String)
+
+    # strip leading and trailing whitespace from sentence
+    sentence = sentence.strip
+
+    # raise exception if empty string
+    raise InvalidString if sentence.empty?
+
+    # split words into array (regex pattern: any whitespace character)
+    words = sentence.split(/\s+/)
+
+    # reverse order of array and join words using " "
+    words.reverse.join(" ")
+  end
 end
--- spec/sentence_spec.rb
+++ spec/sentence_spec.rb
@@ -5,7 +5,26 @@ describe Sentence do
     expect(Sentence::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  it 'raises an InvalidString exception when a non-string or empty string is provided' do
+    expect {Sentence.reverse(5)}.to raise_error(Sentence::InvalidString)
+    expect {Sentence.reverse([1,2,3])}.to raise_error(Sentence::InvalidString)
+    expect {Sentence.reverse(true)}.to raise_error(Sentence::InvalidString)
+    expect {Sentence.reverse("")}.to raise_error(Sentence::InvalidString)
+    expect {Sentence.reverse("  ")}.to raise_error(Sentence::InvalidString)
+    expect {Sentence.reverse("Single")}.not_to raise_error
+    expect {Sentence.reverse("Multiple words in sentence")}.not_to raise_error
+  end
+
+  it 'returns reversed sentence when provided a non-empty string' do
+    expect(Sentence.reverse("A")).to eq("A")
+    expect(Sentence.reverse("  A")).to eq("A")
+    expect(Sentence.reverse("A  ")).to eq("A")
+    expect(Sentence.reverse("  A  ")).to eq("A")
+    expect(Sentence.reverse("\tABC")).to eq("ABC")
+    expect(Sentence.reverse("ABC\t")).to eq("ABC")
+    expect(Sentence.reverse("\tABC\t")).to eq("ABC")
+    expect(Sentence.reverse("Two Words")).to eq("Words Two")
+    expect(Sentence.reverse("  Two  Words  ")).to eq("Words Two")
+    expect(Sentence.reverse("the big black cat")).to eq("cat black big the")
   end
 end
